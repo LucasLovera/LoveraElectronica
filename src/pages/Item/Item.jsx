2a92eace355bdfedc035a035ae87512b8642ffdb
LucasLovera <lucaslovera.92@gmail.com>
2023-01-18 22:08:58 -0300

    SegundaEntrega2

--- src/pages/Item/Item.jsx
+++ src/pages/Item/Item.jsx
@@ -3,6 +3,7 @@ import ItemList from "../../component/ItemList/ItemLIst";
 import { useParams } from "react-router-dom";
 const ItemLisContainer = ({}) => {
   const [products, setProducts] = useState([]);
+  const [filterProduct, setFilterProduct]= useState([])
   const {category} = useParams();
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -15,15 +16,26 @@ const ItemLisContainer = ({}) => {
       })
       .catch((error) => console.log(error));
   }, []);
-  useEffect(() => {
-    const filtroProductos = products.filter(
-      (product) => product.category === category
-    );
-    setProducts(filtroProductos);
-  }, [category]);
+  // useEffect(() => {
+  //   const filtroProductos = products.filter(
+  //     (product) => product.category === category
+  //   );
+  //   setProducts(filtroProductos);
+  // }, [category]);
+  useEffect (()=>{
+    if(category){
+      const removeCaracter=category.includes('%20')
+      ? category.replace('%20',' ')
+      : category;
+      const filterProduct = products.filter((product)=>{
+        return product.category === removeCaracter;
+      });
+      setFilterProduct(filterProduct)
+    }
+  },[category])
   return (
     <div>
-      <ItemList productos={products} />
+      <ItemList productos={category ? filterProduct: products} />
     </div>
   );
 };
--- src/pages/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/pages/ItemDetailContainer/ItemDetailContainer.jsx
@@ -5,7 +5,6 @@ import ItemDetail from "../../ItemDetail/ItemDetailMap";
 const ItemDetailContainer = ({}) => {
   const [products, setProducts] = useState({});
   const [filterProduct, serFilterProduct]= useState([])
-  const {category} = useParams ();
   const {id}= useParams();
   const getProduct =  fetch(`https://fakestoreapi.com/products/${id}`, { method: 'GET' ,});
   useEffect(() => {
@@ -19,20 +18,9 @@ const ItemDetailContainer = ({}) => {
       })
       .catch((error) => console.log(error));
   }, []);
-  useEffect (()=>{
-    if(category){
-      const removeCaracter=category.includes('%20')
-      ? category.replace('%20',' ')
-      : category;
-      const filterProduct = products.filter((product)=>{
-        return product.category === removeCaracter;
-      });
-      serFilterProduct(filterProduct)
-    }
-  },[category])
   return (
     <div>
-      <ItemDetail productos={category ? filterProduct: products} />
+      <ItemDetail productos={products} />
     </div>
   );
 };
