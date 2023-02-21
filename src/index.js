7e48fd534319eeff36ab148a3145ecb6529295e5
LucasLovera <lucaslovera.92@gmail.com>
2023-02-21 19:32:35 -0300

    trantadoDearreaglarFIrestore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 // Import the functions you need from the SDKs you need
 // Import the functions you need from the SDKs you need
 // Import the functions you need from the SDKs you need
@@ -13,19 +13,19 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_XFNLE5P4j7aeeSIq0tSJu4DLix7wcIU",
-  authDomain: "midi-393fe.firebaseapp.com",
-  projectId: "midi-393fe",
-  storageBucket: "midi-393fe.appspot.com",
-  messagingSenderId: "273105147201",
-  appId: "1:273105147201:web:025503f6ab043de82a3dc0"
-};
-
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};const log= ()=>
+{console.log( firebaseConfig)}
+log()
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
