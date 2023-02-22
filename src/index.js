dcd0afcc2fb66f9f182bc4f1e89a67011abf86a4
LucasLovera <lucaslovera.92@gmail.com>
2023-02-21 22:14:24 -0300

    firebase

--- src/index.js
+++ src/index.js
@@ -19,9 +19,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};const log= ()=>
-{console.log( firebaseConfig)}
-log()
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
